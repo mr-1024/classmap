f415b115ad50aa0650dc25d68e8aeac0b9fc5edd
mr.wind <zouxiuhui@vchangyi.com>
2019-03-19 19:21:38 +0800

    classmap 实例

---
+++ src/Some.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2019/3/19
+ * Time: 19:09
+ */
+class Some
+{
+    public function index()
+    {
+        echo 'HELLO CLASSMAP';
+    }
+}
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 2019/3/19
+ * Time: 19:11
+ */
+require 'vendor/autoload.php';
+
+$test = new Some();
+$test->index();
